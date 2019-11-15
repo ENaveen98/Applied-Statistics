2e704c3a69548d116690e3c20668a3b3e6020de5
Naveen <42776989+ENaveen98@users.noreply.github.com>
2019-11-15 08:29:14 +0530

    Add files via upload

---
+++ 8_5.R
@@ -0,0 +1,27 @@
+# Load the dependent libraries
+library(Bolstad)
+
+# (a)
+pi_values = seq(from = 0, to = 1, by=0.0001)
+pi_prior = rep(1,length(pi_values))
+output_1 = binogcp(x = 7, n = 20, density="user", pi = pi_values, pi.prior = pi_prior)
+
+# (b)
+
+# Finding the posterior mean and standard deviation of pi using integration..
+E_posterior = sintegral(pi_values,pi_values*output_1$posterior, n.pts=length(pi_values))
+mean_posterior = E_posterior$value
+cat("Posterior mean of pi =", mean_posterior, '\n')
+Variance_posterior = sintegral(pi_values,(pi_values-mean_posterior)^2*output_1$posterior, n.pts=length(pi_values))
+stddev_posterior = Variance_posterior$value^0.5
+cat("Posterior standard deviation of pi =", stddev_posterior, '\n')
+
+# (c)
+# Finding Lower and Upper bound for 95% Credible Interval by calculating cdf.
+cdf_1 = sintegral(pi_values,output_1$posterior, n.pts=length(pi_values))
+plot(cdf_1$cdf, type='l', xlab = 'pi', ylab = 'cdf', main = 'Posterior - cdf')
+cdf = cdf_1$cdf
+lower_bound = cdf$x[with(cdf,which.max(x[y<=0.025]))]
+upper_bound = cdf$x[with(cdf,which.max(x[y<=0.975]))]
+cat(paste("Approximate 95% credible interval: [", round(lower_bound, 4),
+          " ", round(upper_bound, 4), "]\n", sep = ""))
